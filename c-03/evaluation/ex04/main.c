5313578b1e033bc89ca20a2acd649656ca56641e
Jonatas Arão de Carvalho Neves <jonatas.arao.de.carvalho.neves@hotmail.com>
2024-08-31 19:58:09 -0300

    c-03/ex04 minior change

--- c-03/evaluation/ex04/main.c
+++ c-03/evaluation/ex04/main.c
@@ -6,11 +6,12 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 16:13:41 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/08/29 11:39:48 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/08/31 19:57:44 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 #include "ft_strstr.h"
 
 char	*ft_strcpy(char *dest, char *src)
@@ -36,16 +37,21 @@ int	main(void)
 	ft_strcpy(str, "Piscina 42 Sao Paulo");
 	ft_strcpy(str2, "42");
 	printf("str = %s\tstr2 = %s\n", str, str2);
-	substr = ft_strstr(str, str2);
+	substr = strstr(str, str2);
 	if (substr)
 		printf("%s encontrado em %s\n", str2, substr);
 	else
 		printf("%s não encontrado\n", str2);
 	ft_strcpy(str2, "SP");
 	printf("str = %s\tstr2 = %s\n", str, str2);
-	substr = ft_strstr(str, str2);
+	substr = strstr(str, str2);
 	if (substr)
 		printf("%s encontrado em %s\n", str2, substr);
+	else
+		printf("%s não encontrado\n", str2);
+	substr = strstr(str, "");
+	if (substr)
+		printf("%s encontrado em %s\n", "Vazio", substr);
 	else
 		printf("%s não encontrado\n", str2);
 	return (0);
